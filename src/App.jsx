e5f4345c832661008940368e948e20c5eb5d5a77
Jerald Labalan <jeraldbalan26@gmail.com>
2024-12-10 06:10:11 +0800

    Add: added header and footer components.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
