627125b1757d7491ff619a6c2caa3de526084c4d
Donna Virtudez <116262219+donnavirtudez@users.noreply.github.com>
2024-12-17 05:58:36 +0800

    test(landing page): add test to ensure all components work as intended

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  setupFilesAfterEnv: ["./setupTests.js"],
+
+  moduleNameMapper: {
+    "^/src/(.*)": "<rootDir>/src/$1",
+    "^src/(.*)$": "<rootDir>/frontend/src/$1",
+    "\\.css$": "identity-obj-proxy",
+    "\\.module\\.css$": "identity-obj-proxy",
+    "\\.(css|less)$": "identity-obj-proxy",
+    "\\.css$": "<rootDir>/styleMock.js",
+    "\\.(css|less|scss|sass)$": "<rootDir>/styleMock.js",
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/fileMock.js",
+  },
+
+  transformIgnorePatterns: [
+    "/node_modules/(?!your-dependency-to-transform)/", // If you're using a module that needs transpiling
+  ],
+
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+    "\\.(jpg|jpeg|png|gif|svg)$": "jest-transform-stub",
+    "^.+\\.css$": "jest-transform-css",
+  },
+
+  testEnvironment: "jsdom",
+
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+};
---
+++ frontend/styleMock.js
@@ -0,0 +1,12 @@
+module.exports = {};
+module.exports = "test-file-stub";
+module.exports = {
+  slide_content_container: 'slide_content_container',
+  container: "container",
+  header: "header",
+  logo_div: "logo_div",
+  logo: "logo",
+  content_search_bar: "content_search_bar",
+  footer: "footer",
+  animate: "animate",
+};
