9a2ef94bb1640d735676aaf1cf8ffcb645881dbd
Donna Virtudez <116262219+donnavirtudez@users.noreply.github.com>
2024-12-23 00:35:16 +0800

    test(footer): fix the describe part and name of the test

--- frontend/src/components/Footer/Footer.test.jsx
+++ frontend/src/components/Footer/Footer.test.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Footer from './Footer';
 
-describe('Footer Component', () => {
-  test('renders the Footer component', () => {
+describe('Unit Testing for the Footer Component', () => {
+  test('Renders the Footer component', () => {
     render(<Footer />);
     const footerElement = screen.getByText(/Footer/i);
     expect(footerElement).toBeInTheDocument();
