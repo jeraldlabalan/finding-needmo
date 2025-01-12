2dcae6350f55af961c1f471c525f7d792e23cca8
Gerlyn Tan <gerlyntan07@gmail.com>
2025-01-12 12:25:21 +0800

    feat: users can change email and password in Account Settings page

--- backend/routes/AccountSettings.js
+++ backend/routes/AccountSettings.js
@@ -21,6 +21,66 @@ db.connect((err) => {
     }
 });
 
+router.post('/changePass/accSettings', (req, res) => {
+    const { confirmNewPass } = req.body;
+
+    bcrypt.hash(confirmNewPass, 10, (err, hashedPassword) => {
+            if (err) {
+                return res.json({ message: "Error hashing password: " + err });
+            }
+    
+            // Insert into the user table with hashed password
+            const update =`
+            UPDATE user
+            SET Password = ?,
+                UpdatedAt = NOW()
+            WHERE Email = ?`;
+            const values = [
+                hashedPassword,  // Store hashed password
+                req.session.email
+            ];
+    
+            db.query(update, values, (err, userResult) => {
+                if (err) {
+                    return res.json({ message: "Error in server: " + err });
+                } else if (userResult.affectedRows > 0) {
+                    return res.json({ message: "Success" });
+                } else {
+                    return res.json({ message: "Failed" });
+                }
+            });
+        });
+})
+
+router.post('/changeEmail/accSettings', (req, res) => {
+    const { email } = req.body;
+
+    const update = `UPDATE user
+                    SET Email = ?,
+                        UpdatedAt = NOW()
+                    WHERE Email = ?`;
+
+    db.query(update, [email, req.session.email], (err, updateRes) => {
+        if (err) {
+            return res.json({ message: "Error in server: " + err });
+        } else if (updateRes.affectedRows > 0) {
+            req.session.destroy((err) => {
+                if (err) {
+                    console.error("Error destroying session:", err);
+                    return res.json({ message: "Logout failed" });
+                }
+
+                res.clearCookie("connect.sid");
+                return res.json({ message: "Success", logout: true });
+            });
+        } else {
+            return res.json({ message: "Failed" });
+        }
+    });
+});
+
+
+
 router.post('/verifyPassword/accSettings', (req, res) => {
     const {password} = req.body;
     const user = `SELECT * FROM user WHERE Email = ?`;
--- frontend/src/components/AccountSettings/AccountSettings.jsx
+++ frontend/src/components/AccountSettings/AccountSettings.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./AccountSettings.module.css";
 import Header from "../Header/Header";
@@ -7,7 +7,11 @@ import verify_icon_black from "../../assets/verify-icon-black.png";
 import email_icon_white from "../../assets/email-icon-white.png";
 import email_icon from '../../assets/email.png'
 import verify_icon from '../../assets/verified_icon.png'
+import logoutFunction from '../logoutFunction.jsx';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function AccountSettings() {
   const [selectedForm, setSelectedForm] = useState(null);
@@ -15,8 +19,37 @@ function AccountSettings() {
   const [currentChangePasswordStep, setCurrentChangePasswordStep] = useState(1);
   const [emailData, setEmailData] = useState({
     password: '',
-
+    email: '',
   });
+
+  const [passData, setPassData] = useState({
+    password: '',
+    newPass: '',
+    confirmNewPass: '',
+  })
+  const [userEmail, setUserEmail] = useState("");
+  const [userRole, setUserRole] = useState('');
+
+  //Reuse in other pages that requires logging in
+  const navigate = useNavigate();
+  axios.defaults.withCredentials = true;
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080")
+      .then((res) => {
+        if (res.data.valid) {
+          setUserEmail(res.data.email);
+          setUserRole(res.data.role);
+          console.log(res.data.role);
+        } else {
+          navigate("/registerlogin");
+        }
+      })
+      .catch((err) => {
+        console.error("Error validating user session:", err);
+      });
+  }, []);
+  //Reuse in other pages that requires logging in
 
   const nextStep = () => {
     setCurrentChangePasswordStep((prev) => (prev < 3 ? prev + 1 : prev));
@@ -34,36 +67,174 @@ function AccountSettings() {
 
   // cancel button logic
   const cancelForm = () => {
-    setSelectedForm(null); 
-    setCurrentChangeEmailStep(1); 
-    setCurrentChangePasswordStep(1); 
+    setSelectedForm(null);
+    setCurrentChangeEmailStep(1);
+    setCurrentChangePasswordStep(1);
+
+    setEmailData({
+      password: '',
+      email: '',
+    });
+
+    setPassData({
+      password: '',
+    newPass: '',
+    confirmNewPass: '',
+    });
   };
 
   const handleEmailChange = (e) => {
     const { name, value } = e.target;
-  setEmailData((prev) => ({
-    ...prev,
-    [name]: value,
-  }));
+    setEmailData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   }
 
-  const handleVerifyPassword = () => {
-    console.log(emailData);
+  const handlePassChange = (e) => {
+    const { name, value } = e.target;
+    setPassData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
 
-    axios.post('http://localhost:8080/verifyPassword/accSettings', {password: emailData.password})
+  //change email
+  const handleVerifyPassword = () => {
+    if (emailData.password === '' || !emailData.password) {
+      toast.error('Input your password', {
+        autoClose: 2000
+      });
+      return;
+    }
+
+    axios.post('http://localhost:8080/verifyPassword/accSettings', { password: emailData.password })
+      .then((res) => {
+        if (res.data.message === "Correct") {
+          setCurrentChangeEmailStep((prev) => (prev < 3 ? prev + 1 : prev));
+          toast.dismiss();
+        } else {
+          toast.error('Your password is wrong', {
+            autoClose: 2000
+          });
+        }
+      })
+      .catch((err) => {
+        toast.error('An error occurred. Please try again.', {
+          autoClose: 2000
+        });
+        console.log("Error: " + err);
+      })
+  }
+
+  const handleSubmitNewEmail = () => {
+    if (!emailData.email || emailData.email === '') {
+      toast.error('Input your current email', {
+        autoClose: 2000
+      });
+      return;
+    }
+
+    if (userRole === "Educator") {
+      const email = emailData.email.trim();
+
+      // Regex for dcs-prefixed email format
+      const validEmailRegex = /^dcs\.[a-zA-Z]+\.[a-zA-Z]+@cvsu\.edu\.ph$/;
+
+      if (!validEmailRegex.test(email)) {
+        toast.error(
+          "Please enter a valid email in the format: dcs.firstname.lastname@cvsu.edu.ph"
+        ), {
+          autoClose: 2000
+        };
+        return;
+      }
+    }
+
+    axios.post('http://localhost:8080/changeEmail/accSettings', { email: emailData.email })
+      .then((res) => {
+        if (res.data.message === "Success") {
+          if (res.data.logout) {
+            toast.dismiss();
+            setCurrentChangeEmailStep((prev) => (prev < 3 ? prev + 1 : prev));
+
+            setTimeout(() => {
+              logoutFunction(navigate);
+            }, 3000);
+          }
+        } else {
+          toast.error('Failed to change email. Kindly reload the page and try again.', {
+            autoClose: 2000
+          });
+        }
+      })
+      .catch((err) => {
+        toast.error('An error occurred. Please try again.', {
+          autoClose: 2000
+        });
+        console.log("Error: " + err);
+      });
+  };
+
+  //change password
+  const handleVerifyPassword1 = () => {
+    if (passData.password === '' || !passData.password) {
+      toast.error('Input your password', {
+        autoClose: 2000
+      });
+      return;
+    }
+
+    axios.post('http://localhost:8080/verifyPassword/accSettings', { password: passData.password })
+      .then((res) => {
+        if (res.data.message === "Correct") {
+          setCurrentChangePasswordStep((prev) => (prev < 3 ? prev + 1 : prev));
+          toast.dismiss();
+        } else {
+          toast.error('Your password is wrong', {
+            autoClose: 2000
+          });
+        }
+      })
+      .catch((err) => {
+        toast.error('An error occurred. Please try again.', {
+          autoClose: 2000
+        });
+        console.log("Error: " + err);
+      })
+  }
+
+  const handleSubmitNewPass = () => {
+    if (passData.newPass === '' || !passData.newPass || passData.confirmNewPass === '' || !passData.confirmNewPass) {
+      toast.error('Input your new password', {
+        autoClose: 2000
+      });
+      return;
+    } else if(passData.newPass !== passData.confirmNewPass){
+      toast.error(`Passwords don't match`, {
+        autoClose: 2000
+      });
+      return;
+    }
+
+    axios.post('http://localhost:8080/changePass/accSettings', { confirmNewPass: passData.confirmNewPass })
     .then((res) => {
-      if(res.data.message === "Correct"){
-        setCurrentChangeEmailStep((prev) => (prev < 3 ? prev + 1 : prev));
+      if(res.data.message === "Success"){
+        setCurrentChangePasswordStep((prev) => (prev < 3 ? prev + 1 : prev));
+        toast.dismiss();
       } else {
-        alert(res.data.message);
+        toast.error('Failed to change password. Kindly reload the page and try again.', {
+          autoClose: 2000
+        });
       }
     })
     .catch((err) => {
-      alert("An error occurred. Please try again.");
+      toast.error('An error occurred. Please try again.', {
+        autoClose: 2000
+      });
       console.log("Error: " + err);
     })
   }
-  
 
   return (
     <div className={styles.container}>
@@ -171,12 +342,15 @@ function AccountSettings() {
                 />
                 <h2 className={styles.content_title}>change email</h2>
                 <p className={styles.content_subtitle}>
-                  Your current email is <span className={styles.bolded_text}>loona@gmail.com</span>. <br />What would you like it to update to?
+                  Your current email is <span className={styles.bolded_text}>{userEmail}</span>. <br />What would you like it to update to?
                 </p>
               </div>
               <div className={styles.email_field_container}>
                 <input
                   type="password"
+                  value={emailData.email}
+                  name='email'
+                  onChange={handleEmailChange}
                   className={styles.email_field}
                   placeholder="New email address"
                 />
@@ -184,7 +358,7 @@ function AccountSettings() {
               <div className={styles.nav_buttons_container}>
                 <button
                   className={`${styles.button_next} ${styles.button}`}
-                  onClick={nextStep}
+                  onClick={handleSubmitNewEmail}
                 >
                   next
                 </button>
@@ -207,12 +381,12 @@ function AccountSettings() {
                 />
                 <h2 className={styles.content_title}>email address changed!</h2>
                 <p className={styles.content_subtitle}>
-                Your email address has been successfully <br />
-                 changed to <span className={styles.bolded_text}>artms@gmail.com</span>
+                  Your email address has been successfully <br />
+                  changed to <span className={styles.bolded_text}>{emailData.email}</span>. Logging out in 3 seconds...
                 </p>
               </div>
               <div className={styles.nav_buttons_container}>
-               
+
                 <button
                   className={`${styles.button_go_back} ${styles.button}`}
                   onClick={cancelForm}
@@ -245,6 +419,9 @@ function AccountSettings() {
               <div className={styles.password_field_container}>
                 <input
                   type="password"
+                  value={passData.email}
+                  name='password'
+                  onChange={handlePassChange}
                   className={styles.password_field}
                   placeholder="Password"
                 />
@@ -252,7 +429,7 @@ function AccountSettings() {
               <div className={styles.nav_buttons_container}>
                 <button
                   className={`${styles.button_next} ${styles.button}`}
-                  onClick={nextStep}
+                  onClick={handleVerifyPassword1}
                 >
                   next
                 </button>
@@ -275,26 +452,25 @@ function AccountSettings() {
                 />
                 <h2 className={styles.content_title}>change password</h2>
                 <p className={styles.content_subtitle_password}>
-                  Update your password by entering your current password. 
-                  <br />
                   Ensure the new password matches the confirmation password.
                 </p>
               </div>
-              <div className={styles.password_field_field_container}>
-                <input
-                  type="password"
-                  className={styles.password_field}
-                  placeholder="Current password"
-                />
+              <div className={styles.password_field_field_container}>                
 
                 <input
                   type="password"
+                  value={passData.newPass}
+                  name='newPass'
+                  onChange={handlePassChange}
                   className={styles.password_field}
                   placeholder="New password"
                 />
 
                 <input
                   type="password"
+                  value={passData.confirmNewPass}
+                  name='confirmNewPass'
+                  onChange={handlePassChange}
                   className={styles.password_field}
                   placeholder="Confirm new password"
                 />
@@ -302,7 +478,7 @@ function AccountSettings() {
               <div className={styles.nav_buttons_container}>
                 <button
                   className={`${styles.button_next} ${styles.button}`}
-                  onClick={nextStep}
+                  onClick={handleSubmitNewPass}
                 >
                   update password
                 </button>
@@ -313,7 +489,7 @@ function AccountSettings() {
                 <Link to="" className={styles.forgot_password}>
                   forgot password?
                 </Link>
-                
+
               </div>
             </div>
           </div>
@@ -323,28 +499,28 @@ function AccountSettings() {
 
         {selectedForm === "password" && currentChangePasswordStep === 3 && (
           <div className={styles.change_password_form}>
-          <div className={styles.reusable_sucess_form}>
-            <div className={styles.content_title_container}>
-              <img
-                src={verify_icon}
-                className={styles.account_setting_icon}
-                alt="account settings icon"
-              />
-              <h2 className={styles.content_title}>password changed!</h2>
-              <p className={styles.content_subtitle}>
-              Your email address has been successfully updated.
-              </p>
-            </div>
-            <div className={styles.nav_buttons_container}>
-              <button
-                className={`${styles.button_go_back} ${styles.button}`}
-                onClick={cancelForm}
-              >
-                go back
-              </button>
+            <div className={styles.reusable_sucess_form}>
+              <div className={styles.content_title_container}>
+                <img
+                  src={verify_icon}
+                  className={styles.account_setting_icon}
+                  alt="account settings icon"
+                />
+                <h2 className={styles.content_title}>password changed!</h2>
+                <p className={styles.content_subtitle}>
+                  Your password has been successfully updated.
+                </p>
+              </div>
+              <div className={styles.nav_buttons_container}>
+                <button
+                  className={`${styles.button_go_back} ${styles.button}`}
+                  onClick={cancelForm}
+                >
+                  go back
+                </button>
+              </div>
             </div>
           </div>
-        </div>
         )}
       </div>
     </div>
